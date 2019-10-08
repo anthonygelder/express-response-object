523f7adf7ecabf49ab2e0566ac4d79a78f8fd212
Anthony Gelder <anthony.gelder@gmail.com>
2019-10-07 18:40:45 -0700

    assignment requirements

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('common'));
 
+const apps = require('./playstore-data.js')
 
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
+app.get('/apps', (req, res) => {
+    const { genres = "", sort} = req.query;
+
+    if (sort) {
+        if (!['Rating', 'App'].includes(sort)) {
+        return res
+            .status(400)
+            .send('Sort must be one of rating or app');
+        }
+    }
+
+    let results = apps.filter(app => app.Genres
+                                        .toLowerCase()
+                                        .includes(genres.toLowerCase()));
+
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        });
+    }
+
+    res.json(results);
 });
-
 
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
